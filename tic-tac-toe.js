0d4767d595393afc12cdf1b4627ddde0416f8a2b
Shantae Saddler <saddler.shantae02@gmail.com>
2022-10-14 21:50:58 -0500

    Added hover function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,7 @@ let track=[]
 function clickAction(ps){
     if (track.length==0 || track[0]=="O"){
         ps.textContent= "X"
-        ps.classList.add("x")
+        ps.classList.add("X")
         track[0]="X"
     }
     else if (track[0]== "X"){
@@ -13,13 +13,22 @@ function clickAction(ps){
     }
 }
 
+function hoveraction(ps){
+    ps.classList.add("hover")
+}
+
+function hoveroutaction(ps){
+    ps.classList.remove("hover")
+}
+
 onload = function(){
     var_div=document.getElementById("board").children
 
     for(let i=0 ;i< var_div.length; i++){
         var_div[i].setAttribute("class","square")
         var_div[i].setAttribute("onclick","clickAction(this)")
+        var_div[i].setAttribute("onmouseover","hoveraction(this)")
+        var_div[i].setAttribute("onmouseout","hoveroutaction(this)")
     }
-
-    console.log(var_div)
 }
+
